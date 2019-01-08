f230d39f69b8b62bec10430b7fedb306a4e53c3d
rufunt <jazzoo@mail.ru>
2019-01-08 19:37:04 +0000

    Add migration for fulname

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190103171452) do
+ActiveRecord::Schema.define(version: 20190108191918) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20190103171452) do
     t.datetime "remember_created_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "fullname"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
